433d4de69358d4c435e04c4941b74680dfce37b1
matianhe3 <matianhe3@gmail.com>
2022-06-24 13:56:56 +0800

    update

--- lua/editor/diffview.lua
+++ lua/editor/diffview.lua
@@ -13,27 +13,6 @@ local function diffview()
       fold_closed = "",
       fold_open = "",
     },
-    file_panel = {
-      width = 35,                         -- Only applies when position is 'left' or 'right'
-      height = 10,                        -- Only applies when position is 'top' or 'bottom'
-      listing_style = "tree",             -- One of 'list' or 'tree'
-      tree_options = {                    -- Only applies when listing_style is 'tree'
-        flatten_dirs = true,              -- Flatten dirs that only contain one single dir
-        folder_statuses = "only_folded",  -- One of 'never', 'only_folded' or 'always'.
-      },
-    },
-    file_history_panel = {
-      width = 35,
-      height = 16,
-      log_options = {
-        max_count = 256,      -- Limit the number of commits
-        follow = false,       -- Follow renames (only for single file)
-        all = false,          -- Include all refs under 'refs/' including HEAD
-        merges = false,       -- List only merge commits
-        no_merges = false,    -- List no merge commits
-        reverse = false,      -- List commits in reverse order
-      },
-    },
     default_args = {    -- Default args prepended to the arg-list for the listed commands
       DiffviewOpen = {},
       DiffviewFileHistory = {},
